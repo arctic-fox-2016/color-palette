490d13dcb51e48dbcd24c99ab12f402242310aea
Andrew <atandiawan@gmail.com>
2016-09-21 19:46:52 +0700

    Selesai

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require("express");
 var app = express();
+var fs = require('fs')
+var getPallete = require('./lib/get_pallete')
 
 app.set('view engine', 'jade');
 
 app.get("/", function (req, res) {
-  res.render("index", { palette: ["#69D2E7", "#E0E4CC", "#F38630"] });
+  res.render("index", getPallete());
 });
 
 console.log("Server runing on http://localhost:9000")
 app.listen(9000);
+
+console.log(getPallete())
